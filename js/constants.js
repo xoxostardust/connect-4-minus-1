fbb3bca64e8bda78df39151ef361d4f90b20d591
gabs <risingmoons16@gmail.com>
2023-03-15 14:09:57 -0700

    added winning arrays to js

--- js/constants.js
+++ js/constants.js
@@ -5,3 +5,19 @@ export const GRID_ROWS = 6;
 // Default game constants
 export const RED_PIECE_COUNT = 21;
 export const YELLOW_PIECE_COUNT = 21;
+
+//Default winning rows for the grid
+const winningArrays = [
+    [1,2,3,4],
+    [2,3,4,5],
+    [3,4,5,6],
+    [1,1,1,1],
+    [2,2,2,2],
+    [3,3,3,3],
+    [4,4,4,4],
+    [5,5,5,5],
+    [6,6,6,6],
+    [6,5,4,3],
+    [5,4,3,2],
+    [4,3,2,1],
+]
